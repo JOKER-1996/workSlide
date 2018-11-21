36028f60eac872ce365da5b4d0ff011ed70c0925
JOKER-1996 <neroexpressgasan@gmail.com>
2018-11-21 16:34:14 +0200

    worksSlider

---
+++ web/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$('.slider').slick({
+		infinite: true,
+		slidesToShow: 3.5,
+		slidesToScroll: 1,
+		prevArrow: '.slider__leftArrow',
+		nextArrow: '.slider__rightArrow'
+	});
+
+});
